341c813655e1a0f997991e1bd5b259704be67673
mgonza14 <61465242+mgonza14@users.noreply.github.com>
2020-02-26 16:26:36 -0600

    Add files via upload

---
+++ DNS_Windows_Change_DNS_Settings.ps1
@@ -0,0 +1,37 @@
+#######################################################################################################
+                                                                                                     
+##   This script will take a text file of server names updates DNS settings via WINRM port 5983.       
+		 
+                                                                                                     
+##	Version Modifications																			  
+##	- 1.2 - Code set to input a text file and parse each line item									  
+##	- 1.1 - Code set to take in an array string within the code										  
+##	- 1.0 - Code set to take in an individual string as input									      																				
+                                                                                                     
+#######################################################################################################
+
+ForEach ($RemoteComputers in Get-Content servers.txt) {
+    $computer = $RemoteComputers
+	# 1. List Computer Name where DNS is being Changed
+	Write-Host "Computer Name: " $Computer  -ForegroundColor Green
+    
+	Invoke-Command -ComputerName $Computer -ScriptBlock {
+        #2. sets new dns servers. Change IP ADDR to the IP address of the DNS servers
+		$NewDnsServerSearchOrder = "IP ADDR, IP ADDR"
+        
+		#3. searches the network interfaces for static settings
+		$Adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.DHCPEnabled -ne 'True' -and $_.DNSServerSearchOrder -ne $null}
+        
+        #4.  Show DNS settings on server before change
+        Write-Host "Before: " -ForegroundColor Green
+        $Adapters | ForEach-Object {$_.DNSServerSearchOrder}
+ 
+        #5. Update DNS servers
+        $Adapters | ForEach-Object {$_.SetDNSServerSearchOrder($NewDnsServerSearchOrder)} | Out-Null
+ 
+        #6.  Show DNS servers on server after update
+        $Adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.DHCPEnabled -ne 'True' -and $_.DNSServerSearchOrder -ne $null}
+		Write-Host "After: " -ForegroundColor Green
+        $Adapters | ForEach-Object {$_.DNSServerSearchOrder}
+    }
+}
