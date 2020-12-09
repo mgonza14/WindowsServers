73836decb380745cc01808b931e8244f4b72b2a5
Gonzalez <MG_Admin@alliantcreditunion.com>
2020-12-09 13:42:27 -0600

    1.3 - added help file and commited to personal git server

--- DNS_Windows_Change_DNS_Settings.ps1
+++ DNS_Windows_Change_DNS_Settings.ps1
@@ -1,14 +1,19 @@
-#######################################################################################################
-                                                                                                     
-##   This script will take a text file of server names updates DNS settings via WINRM port 5983.       
-		 
-                                                                                                     
+<#
+.SYNOPSIS
+    DNS change script
+.DESCRIPTION
+    This script will take a text file of server names updates DNS settings via WINRM port 5983.       
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.NOTES
 ##	Version Modifications																			  
 ##	- 1.2 - Code set to input a text file and parse each line item									  
 ##	- 1.1 - Code set to take in an array string within the code										  
 ##	- 1.0 - Code set to take in an individual string as input									      																				
                                                                                                      
 #######################################################################################################
+#>
 
 ForEach ($RemoteComputers in Get-Content servers.txt) {
     $computer = $RemoteComputers
